17272bc4a95d878a38ff93c5368880fa3ef114e5
Marwa Asad <100716784+marwaasad@users.noreply.github.com>
2023-11-23 11:16:50 +0530

    Update authMiddleware.js

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,32 +1,36 @@
 // authMiddleware.js
-const {jwtDecode} = require('jwt-decode');
+const { jwtDecode } = require('jwt-decode');
+import 'core-js/stable/atob';
+
+function atob(input) {
+  return Buffer.from(input, 'base64').toString('binary');
+}
 
 function getUserIdFromToken(authorizationHeader) {
-    try {
-      // Ensure authorizationHeader is a string
-      if (typeof authorizationHeader !== 'string') {
-        throw new Error('Authorization header is not a string');
-      }
-  
-      const tokenParts = authorizationHeader.trim().split(' ');
-  
-      // Ensure the correct format of the authorization header
-      if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
-        throw new Error('Invalid format of the authorization header');
-      }
-  
-      const token = tokenParts[1];
-  
-      // Decode the token and return the userId
-      const decodedToken = jwtDecode(token);
-      return decodedToken.userId;
-    } catch (error) {
-      console.error('Error in getUserIdFromToken:', error.message);
-      throw new Error('Error decoding token');
+  try {
+    // Ensure authorizationHeader is a string
+    if (typeof authorizationHeader !== 'string') {
+      throw new Error('Authorization header is not a string');
     }
-  }
 
-  
+    const tokenParts = authorizationHeader.trim().split(' ');
+
+    // Ensure the correct format of the authorization header
+    if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
+      throw new Error('Invalid format of the authorization header');
+    }
+
+    const token = tokenParts[1];
+
+    // Decode the token and return the userId
+    const decodedToken = jwtDecode(atob(token)); // Use the atob replacement here
+    return decodedToken.userId;
+  } catch (error) {
+    console.error('Error in getUserIdFromToken:', error.message);
+    throw new Error('Error decoding token');
+  }
+}
+
 function jwtMiddleware(req, res, next) {
   const authHeader = req.headers.authorization;
 
